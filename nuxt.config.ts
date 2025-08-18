62e0a0e8fd86aa0a275929e627cb6d8a820d5d01
Talkird <losaurojuan@gmail.com>
2025-08-18 16:42:28 -0300

    folder structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@pinia/nuxt']
-})
+  modules: ["@nuxt/eslint", "@nuxt/ui", "@pinia/nuxt"],
+  css: ["~/main.css"],
+});
