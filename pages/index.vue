d6791a3b7a7a3bfaa76b62e158e16fba68e484e8
Juan Andrés Losauro <losaurojuan@gmail.com>
2025-09-01 16:21:37 -0300

    seo test

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "Welcome to cineTrack",
+});
+</script>
+
 <template>
   <div class="from-nav to-footer flex min-h-screen bg-gradient-to-t"></div>
 </template>
