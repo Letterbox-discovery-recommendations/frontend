cbc4297e7397b5dafafb9d5ce2d05c53de05510f
Juan Andrés Losauro <losaurojuan@gmail.com>
2025-09-01 16:19:15 -0300

    seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,6 @@ export default defineNuxtConfig({
   ssr: false,
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxt/image"],
+  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxt/image", "@nuxtjs/seo"],
   css: ["~/main.css"],
 });
