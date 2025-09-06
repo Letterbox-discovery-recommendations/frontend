fca9f0c069caf12e9422ed5b2553bb1b7f551735
Juan Andrés Losauro <87399338+Talkird@users.noreply.github.com>
2025-09-06 13:45:33 -0300

    Dyr 57 implementar la interfaz de usuario para mostrar ra (#7)
    * movie component
    
    * MovieCarousel component, title and separator
    
    * Added MovieCarousel to tops page
    
    * Top filters, SEO
    
    * eslint fix
    
    * update

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 useSeoMeta({
-  title: "Welcome to cineTrack",
+  title: "cineTrack - Inicio",
+  description: "Descubrí las mejores películas.",
 });
 </script>
 
 <template>
-  <div class="from-nav to-footer flex min-h-screen bg-gradient-to-t"></div>
+  <div class="from-nav to-footer flex bg-gradient-to-t"></div>
 </template>
