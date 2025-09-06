51816e77a528d61f28cc71c4fe31b3865e951afd
mathiuSpv <mathius998@gmail.com>
2025-09-06 17:05:27 -0300

    nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: true,
+  ssr: false,
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxt/image", "@nuxtjs/seo"],
   css: ["~/main.css"],
   nitro: {
+    preset: 'static',
     prerender: {
+      routes: ['/'],
       ignore: ['/__sitemap__/style.xsl']
     }
-  }
+  },
+  hooks: {
+    'prerender:routes'({ routes }) {
+      routes.clear()
+    }
+  },
 });
