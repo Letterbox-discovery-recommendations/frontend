5046fb6cfc02d2b8f84c6229735187a295029ed1
Juan Andrés Losauro <losaurojuan@gmail.com>
2025-09-08 20:05:42 -0300

    Nuxt content filters working

--- stores/filter.js
+++ stores/filter.js
@@ -2,14 +2,33 @@ import { defineStore } from "pinia";
 
 export const useFilterStore = defineStore("filter", {
   state: () => ({
-    filters: {},
+    filters: {
+      genres: [],
+      estreno: [],
+      rating: null,
+      duracion: null,
+      plataforma: [],
+    },
   }),
   actions: {
+    setGenres(genres) {
+      this.filters.genres = genres || [];
+    },
     setFilters(newFilters) {
-      this.filters = newFilters.flat();
+      this.filters = { ...this.filters, ...newFilters };
     },
     clearFilters() {
-      this.filters = {};
+      this.filters = {
+        genres: [],
+        estreno: [],
+        rating: null,
+        duracion: null,
+        plataforma: [],
+      };
     },
+  },
+  getters: {
+    hasGenreFilters: (state) =>
+      state.filters.genres && state.filters.genres.length > 0,
   },
 });
